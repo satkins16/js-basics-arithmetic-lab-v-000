067acc6d1d51b1a979238b2a36b65b14c7dfc597
Scott Atkins <satkins16@gmail.com>
2017-10-23 13:05:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,5 @@ const ageIsValid = Number.isInteger(currentAge);
 const randomNumber = Math.floor(Math.random() * 20) + 1;
 
 const randomInteger = Math.floor(randomNumber);
+
+const randomUserID = 10000000 + randomInteger
